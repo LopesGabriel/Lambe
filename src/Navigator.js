bc8f9ae8f4454a883b83259ad5bb825a5217e639
Gabriel Lopes <lopesgabriel0199@gmail.com>
2020-08-12 20:15:49 -0300

    Tela de Adicionar foto e nome do App

--- src/Navigator.js
+++ src/Navigator.js
@@ -4,6 +4,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import Icon from 'react-native-vector-icons/FontAwesome'
 
 import Feed from './screens/Feed'
+import AddPhoto from './screens/AddPhoto'
 
 const Tab = createBottomTabNavigator();
 
@@ -14,15 +15,15 @@ export default function Navigator() {
                 tabBarOptions={{ showLabel: false }} initialRouteName="Feed">
                 <Tab.Screen
                     name="Feed" component={Feed} options={{ title: 'Feed', tabBarBadge: 2, tabBarIcon: ({ color }) =>
-                    <Icon name='home' size={25} color={color} /> }} />
+                    <Icon name='home' size={30} color={color} /> }} />
                 <Tab.Screen
-                    name="AddPhoto" component={Feed} 
+                    name="AddPhoto" component={AddPhoto} 
                     options={{ title: 'Add Picture', tabBarIcon: ({ color }) =>
-                        <Icon name='camera' size={25} color={color} /> }} />
+                        <Icon name='camera' size={30} color={color} /> }} />
                 <Tab.Screen
                     name="Profile" component={Feed} 
                     options={{ title: 'Profile', tabBarIcon: ({ color }) =>
-                        <Icon name='user' size={25} color={color} /> }} />
+                        <Icon name='user' size={30} color={color} /> }} />
             </Tab.Navigator>
         </NavigationContainer>
     )
