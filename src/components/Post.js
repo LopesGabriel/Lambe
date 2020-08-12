0aa9a2aaffe8fe0f4875cf219745a883f6e177f8
Gabriel Lopes <lopesgabriel0199@gmail.com>
2020-08-11 22:15:31 -0300

    Componente Author

--- src/components/Post.js
+++ src/components/Post.js
@@ -5,12 +5,14 @@ import {
     Image,
     Dimensions
 } from 'react-native'
+import Author from './Author'
 
 class Post extends Component {
     render() {
         return (
             <View style={styles.container}>
                 <Image source={this.props.image} style={styles.image} />
+                <Author email='lopesgabriel0199@gmail.com' nickname='Gabriel de Oliveira Lopes' />
             </View>
         )
     }
