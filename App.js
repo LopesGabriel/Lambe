a29bda84316715d4acbd600eb6a6eb9ac279ae77
Gabriel Lopes <lopesgabriel0199@gmail.com>
2020-08-11 22:25:47 -0300

    Componente Comments

--- App.js
+++ App.js
@@ -5,10 +5,19 @@ import Post from './src/components/Post'
 
 export default class App extends Component {
   render() {
+
+    const comments =[{
+      nickname: 'Matheus de Oliveira Lopes',
+      comment: 'Bela foto!'
+    }, {
+      nickname: 'Ivaneide Lopes',
+      comment: 'Eita! Deve estar muito quente ai!!'
+    }]
+
     return (
       <View style={{flex: 1}}>
         <Header />
-        <Post image={require('./assets/imgs/fence.jpg')} />
+        <Post image={require('./assets/imgs/fence.jpg')} comments={comments} />
       </View>
     )
   }
--- src/components/Post.js
+++ src/components/Post.js
@@ -6,6 +6,7 @@ import {
     Dimensions
 } from 'react-native'
 import Author from './Author'
+import Comments from './Comments'
 
 class Post extends Component {
     render() {
@@ -13,6 +14,7 @@ class Post extends Component {
             <View style={styles.container}>
                 <Image source={this.props.image} style={styles.image} />
                 <Author email='lopesgabriel0199@gmail.com' nickname='Gabriel de Oliveira Lopes' />
+                <Comments comments={this.props.comments} />
             </View>
         )
     }
