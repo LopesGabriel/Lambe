94f62e70bab43ac79dff5197fb1cf1935561efc3
Gabriel Lopes <lopesgabriel0199@gmail.com>
2020-08-13 21:12:24 -0300

    Tela de Login + createSwitchNavigator

--- src/Navigator.js
+++ src/Navigator.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
+import { createSwitchNavigator } from '@react-navigation/compat'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import Icon from 'react-native-vector-icons/FontAwesome'
 
 import Feed from './screens/Feed'
 import AddPhoto from './screens/AddPhoto'
 import Profile from './screens/Profile'
+import Login from './screens/Login'
 
 const Tab = createBottomTabNavigator();
+
+const loginOrProfileRouter = createSwitchNavigator({
+    Profile:  Profile,
+    Auth: Login
+},{
+    initialRouteName: 'Profile'
+})
 
 export default function Navigator() {
     return (
@@ -22,7 +31,7 @@ export default function Navigator() {
                     options={{ title: 'Add Picture', tabBarIcon: ({ color }) =>
                         <Icon name='camera' size={30} color={color} /> }} />
                 <Tab.Screen
-                    name="Profile" component={Profile} 
+                    name="Profile" component={loginOrProfileRouter} 
                     options={{ title: 'Profile', tabBarIcon: ({ color }) =>
                         <Icon name='user' size={30} color={color} /> }} />
             </Tab.Navigator>
